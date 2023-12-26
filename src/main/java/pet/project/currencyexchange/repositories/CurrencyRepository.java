32a2dd45b354578db349ec93ab31f5a0da29f543
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-26 13:08:40 +0300

    Add @Repository

--- src/main/java/pet/project/currencyexchange/repositories/CurrencyRepository.java
+++ src/main/java/pet/project/currencyexchange/repositories/CurrencyRepository.java
@@ -1,10 +1,12 @@
 package pet.project.currencyexchange.repositories;
 
+import org.springframework.stereotype.Repository;
 import pet.project.currencyexchange.model.Currency;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CurrencyRepository {
     List<Currency> findAll();
 
