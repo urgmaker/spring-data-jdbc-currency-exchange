63cc193b54ad1e6e1e1843d5c9743384216d18a1
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-26 13:08:13 +0300

    Add CurrencyRepositoryImpl

---
+++ src/main/java/pet/project/currencyexchange/repositories/CurrencyRepositoryImpl.java
@@ -0,0 +1,49 @@
+package pet.project.currencyexchange.repositories;
+
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+import pet.project.currencyexchange.model.Currency;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class CurrencyRepositoryImpl implements CurrencyRepository, RowMapper<Currency> {
+    private final JdbcOperations jdbcOperations;
+
+    public CurrencyRepositoryImpl(JdbcOperations jdbcOperations) {
+        this.jdbcOperations = jdbcOperations;
+    }
+
+    @Override
+    public List<Currency> findAll() {
+        return this.jdbcOperations.query("""
+                select * from c_currencies
+                """, this);
+    }
+
+    @Override
+    public void save(Currency currency) {
+        this.jdbcOperations.update("""
+                insert into c_currencies(id, c_code, c_full_name, c_sign) values (?, ?, ?, ?)
+                """, new Object[]{currency.getId(), currency.getCode(), currency.getFullName(), currency.getSign()});
+    }
+
+    @Override
+    public Optional<Currency> findByCode(String code) {
+        return this.jdbcOperations.query("""
+                        select * from c_currencies where code = ?
+                        """, new Object[]{code}, this)
+                .stream().findFirst();
+    }
+
+    @Override
+    public Currency mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Currency(
+                rs.getInt("id"),
+                rs.getString("c_code"),
+                rs.getString("c_full_name"),
+                rs.getString("c_sign"));
+    }
+}
