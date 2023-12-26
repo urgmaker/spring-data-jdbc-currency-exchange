7c349c7642dd4525c5a35ab04bb36a8564139d11
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-26 16:33:34 +0300

    Add scripts

---
+++ src/test/resources/sql/currencies_rest_controller/test_data.sql
@@ -0,0 +1,3 @@
+insert into c_currencies(c_id, c_code, c_full_name, c_sign)
+values ('5d730ada-9c91-11ed-bef5-631db7f28980', 'USD', 'US Dollar', $),
+       ('5da0a544-9c91-11ed-a601-23bef39fb8a6', 'EUR', 'Euro', '€');
