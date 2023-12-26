a0351df2805f4736583b7bd04edbc2ba905cdfbe
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-26 14:08:02 +0300

    Add Currency

--- src/main/java/pet/project/currencyexchange/model/Currency.java
+++ src/main/java/pet/project/currencyexchange/model/Currency.java
@@ -1,17 +1,23 @@
 package pet.project.currencyexchange.model;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Currency {
-    private Integer id;
+    private UUID id;
     private String code;
     private String fullName;
     private String sign;
+
+    public Currency(String code, String fullName, String sign) {
+    }
 }
