8336f1a6cf0632ece81f5dbea80157986a85c058
Уранов Роман <roman.uranov@gazprombank.ru>
2023-12-27 15:57:55 +0300

    Add CurrencyController

--- src/main/java/pet/project/currencyexchange/controllers/CurrencyController.java
+++ src/main/java/pet/project/currencyexchange/controllers/CurrencyController.java
@@ -1,5 +1,6 @@
 package pet.project.currencyexchange.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import pet.project.currencyexchange.model.Currency;
+import pet.project.currencyexchange.model.NewCurrencyPayload;
 import pet.project.currencyexchange.repositories.CurrencyRepositoryImpl;
 import pet.project.currencyexchange.util.ErrorsPresentation;
 
@@ -20,6 +22,7 @@ public class CurrencyController {
     private final CurrencyRepositoryImpl currencyRepository;
     private final MessageSource messageSource;
 
+    @Autowired
     public CurrencyController(CurrencyRepositoryImpl currencyRepository, MessageSource messageSource) {
         this.currencyRepository = currencyRepository;
         this.messageSource = messageSource;
@@ -35,12 +38,12 @@ public class CurrencyController {
     @PostMapping("currencies")
     @Transactional
     public ResponseEntity<?> handleAddCurrency(
-            @RequestBody Currency payloadCurrency,
+            @RequestBody NewCurrencyPayload payloadCurrency,
             UriComponentsBuilder uriComponentsBuilder,
             Locale locale) {
-        if (payloadCurrency.getCode() == null || payloadCurrency.getCode().isBlank() &&
-                payloadCurrency.getFullName() == null || payloadCurrency.getFullName().isBlank() &&
-                payloadCurrency.getSign() == null || payloadCurrency.getSign().isBlank()) {
+        if ((payloadCurrency.getCode() == null || payloadCurrency.getCode().isBlank()) &&
+            (payloadCurrency.getFullName() == null || payloadCurrency.getFullName().isBlank()) &&
+            (payloadCurrency.getSign() == null || payloadCurrency.getSign().isBlank())) {
             final String message = this.messageSource.getMessage(
                     "currencies.create.details.errors.not_set",
                     new Object[0], locale);
