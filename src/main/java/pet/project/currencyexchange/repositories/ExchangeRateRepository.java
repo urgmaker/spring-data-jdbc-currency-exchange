28dd6644b4373cbf4115a0111fac57c6080737c0
Уранов Роман <roman.uranov@gazprombank.ru>
2024-01-01 09:13:05 +0300

    Add ExchangeRateRepository

--- src/main/java/pet/project/currencyexchange/repositories/ExchangeRateRepository.java
+++ src/main/java/pet/project/currencyexchange/repositories/ExchangeRateRepository.java
@@ -5,12 +5,29 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pet.project.currencyexchange.model.ExchangeRate;
+import pet.project.currencyexchange.util.ExchangeRateRowMapper;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ExchangeRateRepository extends CrudRepository<ExchangeRate, Long> {
-    @Query("SELECT * FROM exchange_rate WHERE base_currency_id = :base_currency_id AND target_currency_id = :target_currency_id")
-    Optional<ExchangeRate> findByBaseIdAndTargetId(@Param("base_currency_id") Integer baseCurrencyId,
-                                                   @Param("target_currency_id") Integer targetCurrencyId);
+    @Query(value = "SELECT er.id as id, er.rate as rate, " +
+                   "bc.id as base_currency_id, bc.code as base_currency_code, bc.full_name as base_currency_full_name, bc.sign as base_currency_sign, " +
+                   "tc.id as target_currency_id, tc.code as target_currency_code, tc.full_name as target_currency_full_name, tc.sign as target_currency_sign " +
+                   "FROM exchange_rate er " +
+                   "JOIN currency bc ON er.base_currency_id = bc.id " +
+                   "JOIN currency tc ON er.target_currency_id = tc.id", rowMapperClass = ExchangeRateRowMapper.class)
+    List<ExchangeRate> findAll();
+
+    @Query(value = "SELECT er.id as id, er.rate as rate, " +
+                   "bc.id as base_currency_id, bc.code as base_currency_code, bc.full_name as base_currency_full_name, bc.sign as base_currency_sign, " +
+                   "tc.id as target_currency_id, tc.code as target_currency_code, tc.full_name as target_currency_full_name, tc.sign as target_currency_sign " +
+                   "FROM exchange_rate er " +
+                   "JOIN currency bc ON er.base_currency_id = bc.id " +
+                   "JOIN currency tc ON er.target_currency_id = tc.id " +
+                   "WHERE bc.code = :base_currency_code AND tc.code = :target_currency_code",
+            rowMapperClass = ExchangeRateRowMapper.class)
+    Optional<ExchangeRate> findByCodes(@Param("base_currency_code") String baseCurrencyCode,
+                                       @Param("target_currency_code") String targetCurrencyCode);
 }
